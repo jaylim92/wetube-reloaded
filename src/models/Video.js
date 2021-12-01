57a28227b1b65d716098f095d239846ce05703d9
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2021-12-01 22:24:40 +0900

    fix #

--- src/models/Video.js
+++ src/models/Video.js
@@ -20,7 +20,7 @@ const videoSchema = new mongoose.Schema({
 videoSchema.static("formatHashtags", function (hashtags) {
   return hashtags
     .split(",")
-    .map((word) => (word.startsWith("#") ? word : `${word}`));
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
