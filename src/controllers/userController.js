5bba7c76c3bc6c3dc228b87f264a00512a740f55
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2021-12-02 23:58:49 +0900

    learned realationship

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
@@ -211,4 +212,14 @@ export const postChangePassword = async (req, res) => {
   //sond notification
   return res.redirect("/users/logout");
 };
-export const see = (req, res) => res.send("See");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id).populate("videos");
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User Not Found" });
+  }
+  return res.render("users/profile", {
+    pageTitle: user.name,
+    user,
+  });
+};
--- src/models/User.js
+++ src/models/User.js
@@ -9,11 +9,12 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: false },
   name: { type: String, required: true },
   location: String,
+  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
 userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 5);
 });
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
