8ef2cf9416a716a565537f172713f3637b3f0535
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2021-12-19 23:31:04 +0900

    isHeroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -163,10 +163,11 @@ export const postEdit = async (req, res) => {
       errorMessage: "User is exists",
     });
   }
+  const isHeroku = process.env.NODE_ENV === "production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.location : avatarUrl,
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
       name,
       email,
       username,
