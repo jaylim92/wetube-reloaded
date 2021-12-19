dbc4a66d8cf15e0a70322238aa2e4c69e9962f64
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2021-12-19 23:44:39 +0900

    change bucket

--- src/middlewares.js
+++ src/middlewares.js
@@ -13,13 +13,13 @@ const isHeroku = process.env.NODE_ENV === "production";
 
 const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "wetubeeeeeeeeeeee/images",
+  bucket: "mywetubechallenge/images",
   acl: "public-read",
 });
 
 const s3VideoUploader = multerS3({
   s3: s3,
-  bucket: "wetubeeeeeeeeeeee/videos",
+  bucket: "mywetubechallenge/videos",
   acl: "public-read",
 });
 
